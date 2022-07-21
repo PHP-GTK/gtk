b1d78df9a02809b750023bd72a2bbd7433ab754e
kmaestro <polishuk1003@gmail.com>
2022-07-21 08:00:56 +0700

    fix timeoutAddSeconds

--- src/GLib/functions.php
+++ src/GLib/functions.php
@@ -1,6 +1,12 @@
 <?php
 
-function timeoutAddSeconds(int $secund, callable $fun): int
+function timeoutAddSeconds(int $secund, callable $fun,  ...$data): int
 {
-    return \PGtk\Gtk\Gtk::getFFI()->g_timeout_add_seconds($secund, $fun, null);
+    return \PGtk\Gtk\Gtk::getFFI()->g_timeout_add_seconds(
+        $secund,
+        function () use ($fun, $data): bool {
+            return $fun(...$data) ?? false;
+        },
+        null
+    );
 }
