949f42e2017300c61cc304d7af161677ea53903d
kmaestro <polishuk1003@gmail.com>
2022-08-08 18:48:19 +0700

    fix exaple

--- example/layout_containers/boxes/main.php
+++ example/layout_containers/boxes/main.php
@@ -6,4 +6,7 @@ require_once __DIR__ . '/BoxWindow.php';
 
 $w = new BoxWindow();
 
+timeoutAddSeconds(1, function ($w) {
+    var_dump('sdf');
+}, $w);
 $w->run();
--- example/simple/hello_world/main.php
+++ example/simple/hello_world/main.php
@@ -7,15 +7,20 @@ $w = new \PGtk\Gtk\Gtk\Window();
 $w->setDefaultSize(600, 600);
 $w->setTitle('Hello world');
 
+$text = new \PGtk\Gtk\Gtk\Text();
+$text->getWidget()->setSizeRequest(100, 100);
+$w->setChild($text);
 $w->widget->show();
 
 
 $run = true;
-$w->widget->GObject->connect('destroy', function () use (&$run) {
-    $run = false;
-});
-while ($run) {
-    \PGtk\Gtk\GLib\MainContext::iteration(true);
-}
+$m = new \PGtk\Gtk\GLib\MainLoop();
+$w->connect('destroy', function ($w, $m) use (&$run) {
+    $m->quit();
+}, $m);
+$m->run();
+//while ($run) {
+//    \PGtk\Gtk\GLib\MainContext::iteration(true);
+//}
 
 
